22f904c5dd01d8c35ed365b00904851d25a47d07
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-10-02 18:03:29 -0300

    Registrar modelos para poder craer posts desde el panel Django

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+#Registramos el modelo para poder crear posts desde el panel de django admin
+from django.contrib import admin 
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha')
+    search_fields =('titulo', 'contenido')
+    
